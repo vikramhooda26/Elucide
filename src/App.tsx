a70baad9e1c5ec1bbca32848a1878bf36371fba0
Anirudh Memani <nemanianirudh@gmail.com>
2024-08-22 02:37:32 +0530

    changed vercel.json file to proxy backend request to same site

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import MainRouter from "./routes/MainRouter";
 import { AppProvider } from "./provider/app-providers";
 
 const App: React.FC = (): React.JSX.Element => {
-    return (
-        <AppProvider>
-            <MainRouter />
-        </AppProvider>
-    );
+	return (
+		<AppProvider>
+			<MainRouter />
+		</AppProvider>
+	);
 };
 
 export default App;
