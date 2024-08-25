e00c382112752d6652b88e7e596478ab19e01f76
MohanKumarMP <mohankumar.mp@hudupu.com>
2024-08-26 03:14:15 +0530

    dashboard APis added to service and master dashboard page

--- src/features/dashboard/tabs/OverView.tsx
+++ src/features/dashboard/tabs/OverView.tsx
@@ -1,14 +1,10 @@
 import { Building, Dumbbell, Trophy, Users } from 'lucide-react';
 import { Card, CardContent, CardHeader, CardTitle } from '../../../components/ui/card';
-import AthleteRecentList from '../athlete/component/AthleteRecentList';
-import BrandRecentList from '../brand/component/BrandRecentList';
-import LeagueRecentList from '../league/component/LeagueRecentList';
-import TeamRecentList from '../team/component/TeamRecentList';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '../../../components/ui/tabs';
+import AthleteDashboard from '../athlete/AthleteDashboard';
 import BrandDashboard from '../brand/BrandDashboard';
 import LeagueDashboard from '../league/LeagueDashboard';
 import TeamDashboard from '../team/TeamDashboard';
-import AthleteDashboard from '../athlete/AthleteDashboard';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '../../../components/ui/tabs';
 
 function OverView() {
 
@@ -27,89 +23,21 @@ function OverView() {
         },
     ];
 
+
+
     return (
-        <div className="space-y-4">
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-                <Card>
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                        <CardTitle className="text-sm font-medium">
-                            Total Brand
-                        </CardTitle>
-                        <Building />
-                    </CardHeader>
-                    <CardContent>
-                        <div className="text-2xl font-bold">450</div>
-                        {/* <p className="text-xs text-muted-foreground">
-                            +20.1% from last month
-                        </p> */}
-                    </CardContent>
-                </Card>
-                <Card>
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                        <CardTitle className="text-sm font-medium">
-                            Total League
-                        </CardTitle>
-                        <Trophy />
-                    </CardHeader>
-                    <CardContent>
-                        <div className="text-2xl font-bold">350</div>
-                        {/* <p className="text-xs text-muted-foreground">
-                            +180.1% from last month
-                        </p> */}
-                    </CardContent>
-                </Card>
-                <Card>
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                        <CardTitle className="text-sm font-medium">Total Team</CardTitle>
-                        <Users />
-                    </CardHeader>
-                    <CardContent>
-                        <div className="text-2xl font-bold">122</div>
-                        {/* <p className="text-xs text-muted-foreground">
-                            +19% from last month
-                        </p> */}
-                    </CardContent>
-                </Card>
-                <Card>
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                        <CardTitle className="text-sm font-medium">
-                            Total Athlete
-                        </CardTitle>
-                        <Dumbbell />
-                    </CardHeader>
-                    <CardContent>
-                        <div className="text-2xl font-bold">573</div>
-                        {/* <p className="text-xs text-muted-foreground">
-                            +201 since last hour
-                        </p> */}
-                    </CardContent>
-                </Card>
-            </div>
-            <Tabs defaultValue="brand" className="space-y-4">
-                <TabsList>
-                    {tabs?.map((tab, i) => (
-                        <TabsTrigger value={tab?.value} key={i}>{tab?.name}</TabsTrigger>
-                    ))}
-                </TabsList>
-                {tabs?.map((content) => (
-                    <TabsContent value={content?.value} className="space-y-4">
-                        {content?.component}
-                    </TabsContent>
+        <Tabs defaultValue="brand" className="space-y-4">
+            <TabsList>
+                {tabs?.map((tab, i) => (
+                    <TabsTrigger value={tab?.value} key={i}>{tab?.name}</TabsTrigger>
                 ))}
-            </Tabs>
-            {/* <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-8">
-                <BrandRecentList />
-            </div>
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-8">
-                <LeagueRecentList />
-            </div>
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-8">
-                <TeamRecentList />
-            </div>
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-8">
-                <AthleteRecentList />
-            </div> */}
-        </div>
+            </TabsList>
+            {tabs?.map((content) => (
+                <TabsContent value={content?.value} className="space-y-4">
+                    {content?.component}
+                </TabsContent>
+            ))}
+        </Tabs>
     )
 }
 
---
+++ src/services/features/DashboardService.ts
@@ -0,0 +1,26 @@
+import { API_URL } from "../../lib/constants";
+import AjaxService from "../AjaxService";
+
+class DashboardService {
+    static master() {
+        return AjaxService.request(API_URL + "/api/admin/dashboard/master", {}, "GET");
+    }
+
+    static brand() {
+        return AjaxService.request(API_URL + "/api/admin/dashboard/brand", {}, "GET");
+    }
+
+    static league() {
+        return AjaxService.request(API_URL + "/api/admin/dashboard/league", {}, "GET");
+    }
+
+    static team() {
+        return AjaxService.request(API_URL + "/api/admin/dashboard/team", {}, "GET");
+    }
+
+    static athlete() {
+        return AjaxService.request(API_URL + "/api/admin/dashboard/athlete", {}, "GET");
+    }
+}
+
+export default DashboardService;
