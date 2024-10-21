b74dc988c95062fa29c448fee5a55dad4f7a325e
AnirudhMemani <nemanianirudh@gmail.com>
2024-10-22 00:08:08 +0530

    chore: remove unused useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,6 @@ import { AppProvider } from "./provider/app-providers";
 import MainRouter from "./routes/MainRouter";
 
 const App: React.FC = (): React.JSX.Element => {
-    useEffect(() => {
-        const currentHost = window.location.hostname;
-        console.log("currentHost", currentHost);
-    }, []);
-
     return (
         <AppProvider>
             <MainRouter />
