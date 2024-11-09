5057c445f77141ca24773adb266c1ce0b584aad5
AnirudhMemani <nemanianirudh@gmail.com>
2024-11-09 22:58:29 +0530

    chore: redeploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { AppProvider } from "./provider/app-providers";
 import MainRouter from "./routes/MainRouter";
 
-const App: React.FC = (): React.JSX.Element => {
+const App: React.FC = (): JSX.Element => {
     return (
         <AppProvider>
             <MainRouter />
